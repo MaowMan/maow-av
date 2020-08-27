f8741a9a0bea3f192818a19c04468f8e1abb9829
建中貓貓男 <54308739+MaowMan@users.noreply.github.com>
2020-08-28 00:57:39 +0800

    8/28

--- app/src/Components/Control.svelte
+++ app/src/Components/Control.svelte
@@ -1,17 +1,24 @@
 <script>
   import { Input, Field, Switch, Tooltip, Button } from "svelma";
-  const sync = {
-    target: ""
-  };
+  const { ipcRenderer } = require("electron");
+  let target = "";
+  async function get_token(url) {
+    const token = await ipcRenderer.invoke("get-token", `${url}`);
+    console.log(token);
+  }
 </script>
 
 <section class="section">
   <div class="columns is-centered">
     <div class="column is-half">
       <Field label="輸入影片網址：">
-        <Input type="text" icon="video" bind:value={sync.target} />
+        <Input type="text" icon="video" bind:value={target} />
       </Field>
-      <Button type="is-primary" iconPack="fas" iconLeft="heart">
+      <Button
+        type="is-primary"
+        iconPack="fas"
+        iconLeft="heart"
+        on:click={() => get_token(target)}>
         開始下載
       </Button>
       <Field />
--- app/src/index.js
+++ app/src/index.js
@@ -1,54 +1,86 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const puppeteer = require('puppeteer-electron');
 const path = require('path');
 require('electron-reload')(__dirname, {
-  electron: path.join(__dirname, '../node_modules', '.bin', 'electron'),
-  awaitWriteFinish: true,
+    electron: path.join(__dirname, '../node_modules', '.bin', 'electron'),
+    awaitWriteFinish: true,
 });
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
+    app.quit();
 }
 
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
-  mainWindow.setMenuBarVisibility(false);
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    mainWindow.setMenuBarVisibility(false);
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, '../public/index.html'));
+    // and load the index.html of the app.
+    mainWindow.loadFile(path.join(__dirname, '../public/index.html'));
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+
+const main = async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const pages = await browser.pages();
+    const [page] = pages
+    await page.goto("https://www.google.com/");
+    let last_ts = null;
+    await page.setRequestInterception(true);
+    page.on('request', request => {
+        if (request.url().split("?")[0].split(".").reverse()[0] === "ts") {
+            last_ts = request.url();
+        }
+        request.continue();
+    });
+    ipcMain.handle("get-token", async (event, arg) => {
+        try {
+            last_ts = null;
+            await page.goto(arg, { waitUntil: 'networkidle0' });
+            return last_ts;
+        }
+        catch (e) {
+            return "error"
+        }
+    })
+    createWindow();
+}
+
+app.on('ready', () => main());
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
+
+
+
+
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
